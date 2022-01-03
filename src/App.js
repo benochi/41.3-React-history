bd1cd25f656bcf627a9116a52d48f545552543fe
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-01-03 08:31:06 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import JokeList from "./JokeList";
+
+/*function App() {
+  return (
+    <div className="App">
+      <JokeList />
+    </div>
+  );
+}*/
+
+//class version
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <JokeList />
+      </div>
+    )
+  }
+}
+export default App;
